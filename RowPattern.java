077b7d67b7a0c1232cfcbc2090489021064cc13a
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 17:51:23 -0500

    Finished Row Pattern Constructor

--- RowPattern.java
+++ RowPattern.java
@@ -5,6 +5,10 @@ public class RowPattern extends BingoPattern {
     public RowPattern() {
         for(int i = 0; i < 5; i++) {
             ArrayList<Coordinates> newPattern = new ArrayList<Coordinates>();
+            for(int j = 0; j < 5; j++) {
+                newPattern.add(new Coordinates(i, j));
+            }
+            validPatterns.add(newPattern);
         }
     }
 }
