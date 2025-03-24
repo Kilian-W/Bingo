4d720bb5bdd3cdaca829a1fbe5c3b7f612e9d850
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 17:49:33 -0500

    Small Code Chunk

--- BingoPattern.java
+++ BingoPattern.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
 public class BingoPattern {
-    
+    protected ArrayList<ArrayList<Coordinates>> validPatterns;
+
+    public boolean getBingo(BingoCard card) {
+        return true;
+    }
 }
--- RowPattern.java
+++ RowPattern.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class RowPattern extends BingoPattern {
     
+    public RowPattern() {
+        for(int i = 0; i < 5; i++) {
+            ArrayList<Coordinates> newPattern = new ArrayList<Coordinates>();
+        }
+    }
 }
