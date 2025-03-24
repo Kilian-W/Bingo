ab1fc58ecdf28a532d780712b91f9c3ecf280d02
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 17:53:53 -0500

    Finished ColPattern Constructor

--- ColPattern.java
+++ ColPattern.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class ColPattern extends BingoPattern {
     
+    public ColPattern() {
+        for(int i = 0; i < 5; i++) {
+            ArrayList<Coordinates> newPattern = new ArrayList<Coordinates>();
+            for(int j = 0; j < 5; j++) {
+                newPattern.add(new Coordinates(j, i));
+            }
+            validPatterns.add(newPattern);
+        }
+    }
 }
