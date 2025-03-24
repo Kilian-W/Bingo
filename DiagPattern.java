90497e49f060a59d8cd0848c4991bac548bb12f2
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 18:00:20 -0500

    finished constructor for DiagPattern

--- DiagPattern.java
+++ DiagPattern.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class DiagPattern extends BingoPattern {
     
+    public DiagPattern() {
+        ArrayList<Coordinates> diagOne = new ArrayList<Coordinates>();
+        ArrayList<Coordinates> diagTwo = new ArrayList<Coordinates>();
+        for(int i = 0; i < 5; i++) {
+            diagOne.add(new Coordinates(i, i));
+            diagTwo.add(new Coordinates(i, 4 - i));
+        }
+        validPatterns.add(diagOne);
+        validPatterns.add(diagTwo);
+    }
 }
