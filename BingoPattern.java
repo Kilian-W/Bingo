1c3ed128530e1b47ae177a3e5c56c8ceed0c9cb9
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 18:15:50 -0500

    More Updates

--- BingoPattern.java
+++ BingoPattern.java
@@ -6,7 +6,14 @@ public class BingoPattern {
     public boolean getBingo(BingoCard card) {
         ArrayList<Coordinates> markedSpaces = card.getMarkedSpaces();
         for(int i = 0; i < validPatterns.size(); i++) {
-
+            ArrayList<Coordinates> currentPattern = validPatterns.get(i);
+            for(int j = 0; j < markedSpaces.size(); j++) {
+                Coordinates currentMarkedSpace = markedSpaces.get(j);
+                for(int k = 0; k < currentPattern.size(); k++) {
+                    Coordinates currentCheckSpace = currentPattern.get(k);
+                    
+                }
+            }
         }
     }
 }
