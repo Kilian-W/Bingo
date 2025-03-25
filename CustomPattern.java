ab17ea912e57ba11cb9310d8bbefcbe7336462b8
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 23:33:39 -0500

    altered Custom pattern

--- CustomPattern.java
+++ CustomPattern.java
@@ -5,4 +5,8 @@ public class CustomPattern extends BingoPattern {
     public void insertCustomPattern(ArrayList<Coordinates> newPattern) {
         validPatterns.add(newPattern);
     }
+
+    public boolean getBingo(BingoCard card) {
+        return super.getBingo(card);
+    }
 }
