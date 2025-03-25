4859a20de7eab399e11c8988eeba945340e8af71
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 23:47:54 -0500

    more copy/pase

--- CustomPatternTest.java
+++ CustomPatternTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
@@ -37,33 +39,58 @@ public class CustomPatternTest {
 
 
         ArrayList<Coordinates> caseOne = new ArrayList<Coordinates>();
-        caseOne.add(new Coordinates(4,4));
-        caseOne.add(new Coordinates(3,3));
-        caseOne.add(new Coordinates(2,2));
-        caseOne.add(new Coordinates(1,1));
-        caseOne.add(new Coordinates(0,0));
+        caseOne.add(new Coordinates(4, 0));
+        caseOne.add(new Coordinates(4, 1));
+        caseOne.add(new Coordinates(4, 2));
+        caseOne.add(new Coordinates(4, 3));
+        caseOne.add(new Coordinates(4, 4));
+        caseOne.add(new Coordinates(3, 2));
+        caseOne.add(new Coordinates(2, 2));
+        caseOne.add(new Coordinates(1, 2));
+        caseOne.add(new Coordinates(0, 2));
         BingoCard testCardOne = new BingoCard();
         testCardOne.setMarkedSpaces(caseOne);
-        boolean testOne = bingoPattern.getBingo(testCardOne);
+        boolean testOne = bingoPatternOne.getBingo(testCardOne);
+        try {
+            Assertions.assertFalse(!testOne);
+        }catch(Exception e) {
+            e.printStackTrace();
+            Assertions.fail("Test One Not Expected To Fail");
+        }
 
         ArrayList<Coordinates> caseTwo = new ArrayList<Coordinates>();
-        caseTwo.add(new Coordinates(0,4));
-        caseTwo.add(new Coordinates(1,3));
-        caseTwo.add(new Coordinates(2,2));
-        caseTwo.add(new Coordinates(3,1));
-        caseTwo.add(new Coordinates(4,0));
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j+) {
+                caseTwo.add(new Coordinates(i, j));
+            }
+        }
         BingoCard testCardTwo = new BingoCard();
         testCardTwo.setMarkedSpaces(caseTwo);
-        boolean testTwo = bingoPattern.getBingo(testCardTwo);
+        boolean testTwo = bingoPatternOne.getBingo(testCardTwo);
+        try {
+            Assertions.assertFalse(!testTwo);
+        }catch(Exception e) {
+            e.printStackTrace();
+            Assertions.fail("Test Two Not Expected To Fail");
+        }
 
         ArrayList<Coordinates> caseThree = new ArrayList<Coordinates>();
-        caseThree.add(new Coordinates(2,0));
-        caseThree.add(new Coordinates(2,1));
-        caseThree.add(new Coordinates(2,2));
-        caseThree.add(new Coordinates(2,3));
-        caseThree.add(new Coordinates(2,4));
+        caseThree.add(new Coordinates(4, 0));
+        caseThree.add(new Coordinates(4, 1));
+        caseThree.add(new Coordinates(4, 2));
+        caseThree.add(new Coordinates(4, 3));
+        caseThree.add(new Coordinates(4, 4));
+        caseThree.add(new Coordinates(3, 2));
+        caseThree.add(new Coordinates(1, 2));
+        caseThree.add(new Coordinates(0, 2));
         BingoCard testCardThree = new BingoCard();
         testCardThree.setMarkedSpaces(caseThree);
-        boolean testThree = bingoPattern.getBingo(testCardThree);
+        boolean testThree = bingoPatternOne.getBingo(testCardThree);
+        try {
+            Assertions.assertFalse(!testThree);
+        }catch(Exception e) {
+            e.printStackTrace();
+            Assertions.fail("Test Three Expected To Fail");
+        }
     }
 }
