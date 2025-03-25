ccae935743b5e9601ae457babd23f6e3910ee5e2
Kilian-W <kilianwilinski@gmail.com>
2025-03-24 23:19:46 -0500

    finishing up test cases for columns

--- ColPatternTest.java
+++ ColPatternTest.java
@@ -10,29 +10,29 @@ public class ColPatternTest {
 
         ArrayList<Coordinates> caseOne = new ArrayList<Coordinates>();
         caseOne.add(new Coordinates(0,0));
-        caseOne.add(new Coordinates(0,1));
-        caseOne.add(new Coordinates(0,2));
-        caseOne.add(new Coordinates(0,3));
-        caseOne.add(new Coordinates(0,4));
+        caseOne.add(new Coordinates(1,0));
+        caseOne.add(new Coordinates(2,0));
+        caseOne.add(new Coordinates(3,0));
+        caseOne.add(new Coordinates(4,0));
         BingoCard testCardOne = new BingoCard();
         testCardOne.setMarkedSpaces(caseOne);
         boolean testOne = bingoPattern.getBingo(testCardOne);
 
         ArrayList<Coordinates> caseTwo = new ArrayList<Coordinates>();
-        caseTwo.add(new Coordinates(2,0));
-        caseTwo.add(new Coordinates(2,1));
-        caseTwo.add(new Coordinates(2,2));
+        caseTwo.add(new Coordinates(0,3));
+        caseTwo.add(new Coordinates(1,3));
         caseTwo.add(new Coordinates(2,3));
-        caseTwo.add(new Coordinates(2,4));
+        caseTwo.add(new Coordinates(3,3));
+        caseTwo.add(new Coordinates(4,3));
         BingoCard testCardTwo = new BingoCard();
         testCardTwo.setMarkedSpaces(caseTwo);
         boolean testTwo = bingoPattern.getBingo(testCardTwo);
 
         ArrayList<Coordinates> caseThree = new ArrayList<Coordinates>();
-        caseThree.add(new Coordinates(4,0));
-        caseThree.add(new Coordinates(4,1));
-        caseThree.add(new Coordinates(4,2));
-        caseThree.add(new Coordinates(4,3));
+        caseThree.add(new Coordinates(0,4));
+        caseThree.add(new Coordinates(1,4));
+        caseThree.add(new Coordinates(2,4));
+        caseThree.add(new Coordinates(3,4));
         caseThree.add(new Coordinates(4,4));
         BingoCard testCardThree = new BingoCard();
         testCardThree.setMarkedSpaces(caseThree);
@@ -43,9 +43,19 @@ public class ColPatternTest {
         caseFour.add(new Coordinates(0,1));
         caseFour.add(new Coordinates(0,2));
         caseFour.add(new Coordinates(0,3));
+        caseFour.add(new Coordinates(0, 4));
         BingoCard testCardFour = new BingoCard();
         testCardFour.setMarkedSpaces(caseFour);
         boolean testFour = bingoPattern.getBingo(testCardFour);
+
+        ArrayList<Coordinates> caseFive = new ArrayList<Coordinates>();
+        caseFive.add(new Coordinates(0,1));
+        caseFive.add(new Coordinates(2,1));
+        caseFive.add(new Coordinates(3,1));
+        caseFive.add(new Coordinates(4, 1));
+        BingoCard testCardFive = new BingoCard();
+        testCardFive.setMarkedSpaces(caseFive);
+        boolean testFive = bingoPattern.getBingo(testCardFive);
 
         try {
             Assertions.assertFalse(!testOne);
@@ -70,6 +80,12 @@ public class ColPatternTest {
 
         try {
             Assertions.assertFalse(testFour);
+        }catch(Exception e) {
+            Assertions.fail("Test Four Expected To Fail");
+        }
+
+        try {
+            Assertions.assertFalse(testFive);
         }catch(Exception e) {
             Assertions.fail("Test Four Expected To Fail");
         }
